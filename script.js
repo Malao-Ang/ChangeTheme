a30a6355722cb4b21405b6221f1d818f2a4bca38
Kaewmanee Marasri <64160234@go.buu.ac.th>
2022-05-14 00:28:52 +0700

    save  the theme went refresh

--- script.js
+++ script.js
@@ -35,12 +35,27 @@ function lightMode() {
 function SwitchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 
 }
 //Event Listener
 toggleSwitch.addEventListener('change', SwitchTheme);
+
+//Check local localStorage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+
+
+}
+console.log(currentTheme);
